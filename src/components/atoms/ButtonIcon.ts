eff72d8e2c40c8c0da63377218660e4fd06c5fc5
najimRizky <najimajim1@gmail.com>
2023-10-17 20:47:22 +0700

    feat(button-icon): create ButtonIcon component

---
+++ src/components/atoms/ButtonIcon.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+import Button from "./Button";
+
+interface Props {
+  size?: "small" | "medium" | "large";
+}
+
+const buttonSize = {
+  small: "2rem",
+  medium: "3rem", 
+  large: "4rem",
+}
+
+const ButtonIcon = styled(Button)<Props>`
+  width: ${({ size }) => buttonSize[size || "medium"]};
+  height: ${({ size }) => buttonSize[size || "medium"]};
+  padding: 0;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.3s ease-in-out;
+`;
+
+export default ButtonIcon;
+
+export type { Props as ButtonIconProps };
