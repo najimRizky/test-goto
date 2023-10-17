7ea277f2f0a4028d0f7813449ed52116a69f2e66
najimRizky <najimajim1@gmail.com>
2023-10-17 19:39:39 +0700

    feat(contact-list): implement ContactCard component

--- src/components/templates/ContactList.tsx
+++ src/components/templates/ContactList.tsx
@@ -1,53 +1,57 @@
 import { FC } from "react"
-import Container from "../atoms/Container"
-import { useMutation, useQuery } from "@apollo/client"
-import { ADD_CONTACT_WITH_PHONES, GET_CONTACT_LIST } from "../../services/contact"
+import {
+  // useMutation,
+  useQuery
+} from "@apollo/client"
+import {
+  // ADD_CONTACT_WITH_PHONES,
+  GET_CONTACT_LIST
+} from "../../services/contact"
+import ContactCard from "../organism/ContactCard"
 
 const ContactList: FC = () => {
-  const { loading, error, data, refetch } = useQuery(GET_CONTACT_LIST, {
-    variables: { limit: 100 },
+  const { loading, error, data } = useQuery(GET_CONTACT_LIST, {
+    variables: { limit: 10 },
   })
 
-  const [addContactWithPhones, { data: dataNew, loading: loadingNew, error: errorNew }] = useMutation(ADD_CONTACT_WITH_PHONES, {
-    refetchQueries: [
-      GET_CONTACT_LIST,
-      "GetContactList"
-    ]
-  })
+  // const [addContactWithPhones, { data: dataNew, loading: loadingNew, error: errorNew }] = useMutation(ADD_CONTACT_WITH_PHONES, {
+  //   refetchQueries: [
+  //     GET_CONTACT_LIST,
+  //     "GetContactList"
+  //   ]
+  // })
 
-  const handleSubmit = async () => {
-    try {
-      await addContactWithPhones({
-        variables: {
-          first_name: "Ersys",
-          last_name: "GG3",
-          phones: [{
-            number: "456789876",
-          }]
-        }
-      })
+  // const handleSubmit = async () => {
+  //   try {
+  //     await addContactWithPhones({
+  //       variables: {
+  //         first_name: "Ersys",
+  //         last_name: "GG3",
+  //         phones: [{
+  //           number: "456789876",
+  //         }]
+  //       }
+  //     })
 
-      alert("Contact added")
-    } catch (error) {
-      console.log(error)
-      alert("Error")
-    }
-  }
+  //     alert("Contact added")
+  //   } catch (error) {
+  //     console.log(error)
+  //     alert("Error")
+  //   }
+  // }
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error </p>
 
   return (
-    <Container>
-      {data?.contact.map((contact: any) => (
-        <div key={contact?.id}>
-          <p>
-            {contact?.first_name} {contact?.last_name}
-          </p>
-        </div>
-      ))}
-      <button onClick={handleSubmit}>Add Contact</button>
-    </Container>
+    <>
+      <main>
+        {data?.contacts.map((contact: any) => (
+          <ContactCard contact={contact} key={contact.id} />
+        ))}
+      </main>
+      {/* <button onClick={handleSubmit}>Add Contact</button> */}
+    </>
   )
 }
 
