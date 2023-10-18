f19a0bf935a4595170c2e363c6a86f0a751d58c5
najimRizky <najimajim1@gmail.com>
2023-10-18 14:55:51 +0700

    feat(flex): create Flex component

---
+++ src/components/atoms/Flex.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled"
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const FlexColumn = styled(Flex)`
+  flex-direction: column;
+`
+
+export const FlexRow = styled(Flex)``
+
+export const FlexJustifyCenter = styled(Flex)`
+  justify-content: center;
+`
+
+export const FlexJustifyBetween = styled(Flex)`
+  justify-content: space-between;
+`
