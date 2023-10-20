1479ccaf39c2bfd6d75b244f12bb589381353859
najimRizky <najimajim1@gmail.com>
2023-10-20 07:01:00 +0700

    refactor: remove unused code

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/organism/Footer'
 
 const BaseLayout = () => {
   return (
     <>
       <Outlet />
-      <Footer />
     </>
   )
 }
--- src/providers/RouterProvider.tsx
+++ src/providers/RouterProvider.tsx
@@ -3,7 +3,6 @@ import BaseLayout from "../layouts/BaseLayout"
 import Home from "../pages/Home"
 import Error from "../pages/Error"
 import Form from "../pages/Form"
-import Detail from "../pages/Detail"
 
 const router = createBrowserRouter([
   {
@@ -18,10 +17,6 @@ const router = createBrowserRouter([
         path: "form",
         element: <Form />
       },
-      {
-        path: ":id",
-        element: <Detail />
-      }
     ]
   }, {
     path: "*",
