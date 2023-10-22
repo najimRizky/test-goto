7eb3ef7d64b6d529ea8af857863148b2d4a4d66c
najimRizky <najimajim1@gmail.com>
2023-10-22 19:40:41 +0700

    refactor(contact_list): change from useNavigate to NavLink and rewrite function

--- src/components/templates/ContactList.tsx
+++ src/components/templates/ContactList.tsx
@@ -4,17 +4,17 @@ import { isAlphaNumeric } from "../../utils/string-helper"
 import { css } from "@emotion/css"
 import FloatingActionButton from "../molecules/FloatingActionButton"
 import PlusIcon from "../../icons/PlusIcon"
-import { useNavigate, useSearchParams } from "react-router-dom"
+import { NavLink, useSearchParams } from "react-router-dom"
 import Spinner from "../atoms/Spinner"
 import Container from "../atoms/Container"
 import Text from "../atoms/Text"
 import Pagination from "../organism/Pagination"
 import { useContact } from "../../providers/ContactProvider"
+import { Contact } from "../../services/contact"
 
 const per_page = 10
 
 const ContactList: FC = () => {
-  const navigate = useNavigate()
   const [searchParam] = useSearchParams()
   const page = parseInt(searchParam.get("page") || "1")
   const search = searchParam.get("search") || ""
@@ -23,14 +23,21 @@ const ContactList: FC = () => {
 
   const { contacts, length } = useMemo(() => getContacts(page, search), [page, search, getContacts])
   const favoriteContacts = useMemo(() => getFavoriteContacts(), [getFavoriteContacts])
+  // const contacts: Contact[] = []
+  // const length = 0
+  // const favoriteContacts: Contact[] = []
   const totalData = length || 0
   const totalPage = Math.ceil(totalData / per_page)
 
-  const renderContactList = useMemo(() => {
-    if (!contacts || contacts?.length === 0) return <Text.P className={css({ textAlign: "center", paddingTop: "8rem" })}>No Contact Found</Text.P>
+  const renderList = (data: Contact[], onEmpty: string) => {
+    if (!data || data?.length === 0) return (
+      <Container>
+        <Text.P >{onEmpty}</Text.P>
+      </Container>
+    )
 
     let initial = ""
-    const contactList = contacts.map((contact) => {
+    const contactList = data.map((contact) => {
       const firstLetter = contact?.first_name[0]?.toUpperCase()
       let showInitial = false
       if (initial !== firstLetter && isAlphaNumeric(firstLetter)) {
@@ -59,41 +66,14 @@ const ContactList: FC = () => {
     })
 
     return contactList
+  }
+
+  const renderContactList = useMemo(() => {
+    return renderList(contacts, "No Contact Found")
   }, [contacts])
 
   const renderFavoriteContactList = useMemo(() => {
-    if (!favoriteContacts || favoriteContacts?.length === 0) return <Text.P className={css({ textAlign: "center", paddingTop: "8rem" })}>No Favorite Contact Found</Text.P>
-
-    let initial = ""
-    const contactList = favoriteContacts.map((contact) => {
-      const firstLetter = contact?.first_name[0]?.toUpperCase()
-      let showInitial = false
-      if (initial !== firstLetter && isAlphaNumeric(firstLetter)) {
-        initial = firstLetter
-        showInitial = true
-      }
-
-      return (
-        <Fragment key={contact.id}>
-          {showInitial &&
-            <Container
-              className={css({
-                padding: "0.25rem 0 0.5rem 1.9rem !important",
-                borderTop: "1px solid transparent",
-                borderBottom: "1px solid var(--gray-light)",
-                fontWeight: 600,
-                color: "var(--gray-dark)",
-                fontSize: "0.875rem",
-              })}
-            >
-              {initial}
-            </Container>}
-          <ContactCard contact={contact} />
-        </Fragment>
-      )
-    })
-
-    return contactList
+    return renderList(favoriteContacts, "No Favorite Contact Found")
   }, [favoriteContacts])
 
   if (loading) return <Spinner spaceY="2rem" />
@@ -102,14 +82,10 @@ const ContactList: FC = () => {
   return (
     <>
       <main className={css({ marginBottom: "2rem", minHeight: "70vh" })}>
-        {favoriteContacts?.length > 0 && (
-          <>
-            <Container>
-              <Text.H2 className={css({ margin: "1rem 0 0.5rem 0" })}>Favorite</Text.H2>
-            </Container>
-            {renderFavoriteContactList}
-          </>
-        )}
+        <Container>
+          <Text.H2 className={css({ margin: "1rem 0 0.5rem 0" })}>Favorite Contact</Text.H2>
+        </Container>
+        {renderFavoriteContactList}
         <Container>
           <Text.H2 className={css({ margin: "1rem 0 0.5rem 0" })}>Regular Contact</Text.H2>
         </Container>
@@ -118,13 +94,14 @@ const ContactList: FC = () => {
       {totalData > 0 && (
         <Pagination page={page} totalPage={totalPage} />
       )}
-      <FloatingActionButton
-        onClick={() => navigate("/form")}
-        isIcon={true}
-        position="bottom-right"
-      >
-        <PlusIcon width={42} />
-      </FloatingActionButton>
+      <NavLink to="/form">
+        <FloatingActionButton
+          isIcon={true}
+          position="bottom-right"
+        >
+          <PlusIcon width={42} />
+        </FloatingActionButton>
+      </NavLink>
     </>
   )
 }
